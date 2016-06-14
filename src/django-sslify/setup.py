de7886765d9e011c6751bea157efd631c60dd335
TopWebGhost <ivanfridrich1981129@yandex.com>
2016-06-15 03:23:40 +0800

    src added

---
+++ src/django-sslify/setup.py
@@ -0,0 +1,52 @@
+"""Python packaging information."""
+
+
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+from sslify import __version__
+
+
+setup(
+
+    # Basic package information:
+    name = 'django-sslify',
+    version = __version__,
+
+    packages = ['sslify'],
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['Django>=1.0'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'r@rdegges.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/django-sslify',
+    keywords = 'django ssl https middleware',
+    description = 'Force SSL on your Django site.',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    long_description = open(normpath(join(dirname(abspath(__file__)), 'README.rst'))).read(),
+
+)
